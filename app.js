ddf807725ebd6b13ff6f686222f6bd66fce2eb0f
Daniele Silva <daniele.silva@ifce.edu.br>
2026-04-24 16:40:32 -0300

    Envio de pastas e codigo

---
+++ app.js
@@ -0,0 +1,48 @@
+function sortear(){
+    let quantidade = document.getElementById("quantidade").value;
+    let de = document.getElementById("de").value;
+    let ate = document.getElementById("ate").value;
+
+    let numero ;
+    
+    let sorteados = [];
+
+     for(let i = 0; i < quantidade; i++){
+        numero = obterNumAleatorio(parseInt(de), parseInt(ate));
+        while(sorteados.includes(numero)){
+            numero = obterNumAleatorio(parseInt(de), parseInt(ate));
+        }
+        sorteados.push(numero);
+            
+    }
+  
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = '<label class="texto__paragrafo">Números sorteados: ' + sorteados.join(", ") + '</label>';
+     alterarStatusBotao();   
+}
+
+function obterNumAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function alterarStatusBotao(){
+    let botao = document.getElementById("btn-reiniciar");
+    if (botao.classList.contains("container__botao-desabilitado")){
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao");
+
+    }else{
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado");
+    }
+   
+}
+function reiniciar(){
+    document.getElementById("quantidade").value = '';
+    document.getElementById("de").value = '';
+    document.getElementById("ate").value = '';
+    document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+
+       
